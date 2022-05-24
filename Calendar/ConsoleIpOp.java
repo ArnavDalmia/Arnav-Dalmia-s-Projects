cdfc3c39372de53921f0c9ad1aace2eee938bf07
Arnav Dalmia <70185864+ArnavDalmia@users.noreply.github.com>
2022-05-24 00:46:47 -0400

    Create ConsoleIpOp.java

---
+++ Calendar/ConsoleIpOp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ConsoleIpOp {
+
+   Scanner obj=new Scanner(System.in);
+   
+   public int getInt()
+   {
+	   int x=obj.nextInt();
+	   return x;
+   }
+}
